01b831477d90da1d99ddd91281f3a8fad85a4f5b
Jeffrey Z <jay.zalischi@gmail.com>
2023-05-22 12:02:16 -0400

    store user data in prisma

--- app/components/providers.tsx
+++ app/components/providers.tsx
@@ -1,5 +1,7 @@
-import { ClerkProvider } from '@clerk/nextjs';
+import { createUser, prisma } from '@/db';
+import { ClerkProvider, currentUser } from '@clerk/nextjs';
 
 export const WithProviders = ({ children }: { children: React.ReactNode }) => {
+  createUser();
   return <ClerkProvider>{children}</ClerkProvider>;
 };
--- db.ts
+++ db.ts
@@ -1,3 +1,31 @@
 import { PrismaClient } from '@prisma/client';
+import { auth, currentUser } from '@clerk/nextjs';
 
+//connect to the database
 export const prisma = new PrismaClient();
+
+// check user auth
+export const legitCheck = () => {
+  const { userId } = auth();
+  if (!userId) {
+    return new Response('Unauthorized', { status: 401 });
+  }
+};
+
+// create user
+export const createUser = async () => {
+  const user = await currentUser();
+
+  if (user) {
+    await prisma.user.upsert({
+      where: {
+        id: user.id,
+      },
+      update: {},
+      create: {
+        id: user.id,
+        email: user.emailAddresses[0].emailAddress,
+      },
+    });
+  }
+};
