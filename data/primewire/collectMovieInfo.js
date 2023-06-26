2d4707fae44db14f8a9417643c6972e48ee44e8b
Jeffrey Z <jay.zalischi@gmail.com>
2023-06-25 20:07:28 -0400

    vercel puppeteer with aws lambda

--- data/primewire/collectMovieInfo.js
+++ data/primewire/collectMovieInfo.js
@@ -1,10 +1,30 @@
-const puppeteer = require('puppeteer');
+let chrome = {};
+let puppeteer;
+if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
+  chrome = require('chrome-aws-lambda');
+  puppeteer = require('puppeteer-core');
+} else {
+  puppeteer = require('puppeteer');
+}
 
 // Example of what the movieLink looks like:
 // fetchMovieIframeURL('https://primewire.mx/watch-movie/watch-spider-man-into-the-spider-verse-sequel-online-66674');
 export const fetchMovieIframeURL = async (movieLink) => {
+  let option = {
+    headless: 'new',
+  };
+  if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
+    option = {
+      headless: true,
+      args: [...chrome.args, '--hide-scrollbars', '--disable-web-security'],
+      defaultViewport: chrome.defaultViewport,
+      executablePath: await chrome.executablePath,
+      ignoreHTTPSErrors: true,
+    };
+  }
+
   console.log(movieLink);
-  const browser = await puppeteer.launch({ headless: 'new' });
+  const browser = await puppeteer.launch(option);
   const page = await browser.newPage();
 
   await page.goto(movieLink);
