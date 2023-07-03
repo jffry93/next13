22c88a2600125b0e047858119d4c736151abc676
Jeffrey Z <jay.zalischi@gmail.com>
2023-07-03 16:28:15 -0400

    rate limite requests to python library

--- app/api/customerSupport/route.ts
+++ app/api/customerSupport/route.ts
@@ -1,12 +1,53 @@
+import { rateLimit } from '@/utils/ratelimiter';
 import { NextResponse } from 'next/server';
 
 // search for movies by title using the movie db api
 export async function GET(req: Request) {
   try {
-    const url = process.env.PYTHON_AI + `/api/sentiment-analysis`;
+    const { success } = await rateLimit.limit('global-rate-limit-key');
 
-    const data = { text: 'Fucking awesome' };
-    console.log(process.env.PYTHON_AI_API_KEY);
+    if (!success) {
+      return NextResponse.json({
+        status: 429,
+        message: 'Too many requests',
+      });
+    }
+
+    const url = process.env.PYTHON_AI + `/api`;
+
+    const res = await fetch(url, {
+      headers: {
+        'Content-Type': 'application/json',
+        'x-api-key': process.env.PYTHON_AI_API_KEY || '', // used to authenticate the request
+      },
+    });
+
+    const json = await res.json();
+
+    return NextResponse.json({
+      data: json,
+      status: { code: 200, message: 'Successful response' },
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+// search for movies by title using the movie db api
+export async function POST(req: Request) {
+  try {
+    const { success } = await rateLimit.limit('global-rate-limit-key');
+
+    if (!success) {
+      return NextResponse.json({
+        status: 429,
+        message: 'Too many requests',
+      });
+    }
+
+    const body = await req.json();
+
+    const url = process.env.PYTHON_AI + `/api/sentiment-analysis`;
 
     const res = await fetch(url, {
       method: 'POST',
@@ -14,12 +55,12 @@ export async function GET(req: Request) {
         'Content-Type': 'application/json',
         'x-api-key': process.env.PYTHON_AI_API_KEY || '', // used to authenticate the request
       },
-      body: JSON.stringify(data),
+      body: JSON.stringify(body),
     });
 
-    const json = await res.text();
+    const json = await res.json();
 
-    return NextResponse.json(json);
+    return NextResponse.json({ data: json });
   } catch (error) {
     console.log(error);
   }
--- app/components/customerSupport/index.tsx
+++ app/components/customerSupport/index.tsx
@@ -3,21 +3,53 @@ import React from 'react';
 import { IoMdHelpBuoy } from 'react-icons/io';
 
 const CustomerSupport = () => {
-  const handleCustomerService = async () => {
+  const customerServiceGET = async () => {
+    console.log('GET');
+
     const res = await fetch('/api/customerSupport');
     const json = await res.json();
     console.log(json);
   };
 
+  const customerServicePOST = async () => {
+    console.log('POST');
+
+    const data = { text: 'i like to party!!' };
+
+    const res = await fetch('/api/customerSupport', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    });
+
+    const json = await res.json();
+
+    console.log(json);
+  };
+
   return (
-    <button
-      className="fixed bottom-6 right-6"
-      onClick={(e) => {
-        handleCustomerService();
-      }}
-    >
-      <IoMdHelpBuoy size={32} color="#bbb" />
-    </button>
+    <>
+      <button
+        className="fixed bottom-6 right-6"
+        onClick={(e) => {
+          customerServicePOST();
+        }}
+      >
+        <IoMdHelpBuoy size={32} color="#bbb" />
+        POST
+      </button>
+      <button
+        className="fixed bottom-6 right-20"
+        onClick={(e) => {
+          customerServiceGET();
+        }}
+      >
+        <IoMdHelpBuoy size={32} color="#bbb" />
+        GET
+      </button>
+    </>
   );
 };
 
